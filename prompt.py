b0db04373a718f1be7b984b4b3b2faf5c5026663
Dipankar Porey <88615645+acfilok96@users.noreply.github.com>
2024-07-17 18:37:25 +0530

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -9,10 +9,4 @@ class PromptCls:
         delimited by triple backticks by giving friendly or funny or angry or \
         sad or surprised or emotional responses.
         """
-        
         return temp_var
-    
-# 5. Do not answer with long phrases and always answer with very short phrases. \
-# 3. Try to add friendly emoji in the middle of response and also at the start & finish point of your response. \
-# 5. Always get straight to the point with a direct and indirect response. \
-# 7. If the message is looking for a website link then reply with the website link otherwise no need to return a website link. \
